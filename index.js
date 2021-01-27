b1f2b241926cd9c8f67e9c52fc4392cadffa4bae
andresamarilis <andres.amarilis@student.uts.edu.au>
2021-01-28 10:16:45 +1100

    activities

---
+++ index.js
@@ -0,0 +1,38 @@
+
+// activity 1
+const addNumber = (numOne, numTwo) => {
+
+return numOne + numTwo; 
+
+}
+
+console.log(addNumber(1, 3)); 
+
+
+// activity 2
+const multiplyNumber = (number) => {
+
+    return number * number; 
+
+}
+console.log(multiplyNumber(3)); 
+
+
+// activity 3
+const greeting = (name) => {
+
+    return "hi " + name; 
+}
+
+console.log(greeting("Jacob")); 
+
+// activity 4
+const upperCaseName = (name) => {
+
+    return name.toUpperCase();
+}
+
+console.log(upperCaseName("greg")); 
+
+
+
